610646007731349c98f1cd269d223b6b7cd5a944
xhilim <56891782+xhilim@users.noreply.github.com>
2024-01-18 00:42:40 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -1,6 +1,18 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
+void dodaj(struct Person** head, const char nLname[], const char nName[], int nage, char npesel[], bool woman);
+void zwolnij(struct Person** head);
 
+void wyswietlListe(struct Person* head);
 
+void wyswietlElement(struct Person* person);
 
+void znajdz(struct Person* head,int x);
+void usun(struct Person** head);
+
+void zapiszDoPliku(struct Person* head);
+void wczytajZPliku(struct Person** head);
+void swap(struct Person* a, struct Person* b);
+void bubbleSort(struct Person* start);
+int strcasecmp(const char* s1, const char* s2);
 #endif // FUNCTIONS
